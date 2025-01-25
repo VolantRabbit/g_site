ca36564e4a493fb79a890c0e2b33305e130974ad
Billy <billytrimkz@gmail.com>
2025-01-25 19:31:42 +0100

    view products on home page

--- og_site/urls.py
+++ og_site/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomePageView
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', views.products, name='products'),
 ]
--- og_site/views.py
+++ og_site/views.py
@@ -1,4 +1,16 @@
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.template import loader
+from og_site.models import Product
 
-class HomePageView(TemplateView):
-    template_name = 'og_site/home.html'
+# class HomePageView(TemplateView):
+#     model = Product
+#     template_name = 'og_site/home.html'
+#     context_object_name = 'products'
+
+def products(request):
+  products = Product.objects.all().values()
+  template = loader.get_template('home.html')
+  context = {
+    'products': products,
+  }
+  return HttpResponse(template.render(context, request))
