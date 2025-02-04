94e52ad06c63d76e47b52aec01cc7b3e168d4fba
Billy <billytrimkz@gmail.com>
2025-02-04 02:30:52 +0100

    images update

--- og_site/urls.py
+++ og_site/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products'),
+    path('', views.main, name='home'),
+    path('products/', views.products, name='products'),
+    path('forum/', views.forum, name='forum'),
 ]
--- og_site/views.py
+++ og_site/views.py
@@ -2,15 +2,21 @@ from django.http import HttpResponse
 from django.template import loader
 from og_site.models import Product
 
-# class HomePageView(TemplateView):
-#     model = Product
-#     template_name = 'og_site/home.html'
-#     context_object_name = 'products'
+
+def main(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
 
 def products(request):
-  products = Product.objects.all()
-  template = loader.get_template('home.html')
-  context = {
-    'products': products,
-  }
-  return HttpResponse(template.render(context, request))
+    products = Product.objects.all()
+    template = loader.get_template('shop.html')
+    context = {
+        'products': products,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def forum(request):
+    template = loader.get_template('forum.html')
+    return HttpResponse(template.render())
